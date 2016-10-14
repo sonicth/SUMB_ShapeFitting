d284a2ab228eaf51cdf4591032531fb2c66a7955
Mike Vasiljevs <mike.vasiljevs@gmail.com>
2016-10-14 14:29:22 +0100

    adaptive algorithm moved into a class

--- src/algorithms/Adaptive.cpp
+++ src/algorithms/Adaptive.cpp
@@ -8,14 +8,10 @@
 #include "generic-algorithms.h"
 
 
-namespace br = boost::range;
 using namespace std;
 using namespace glm;
-namespace bg = boost::geometry;
 using namespace Algorithms;
 
-//TODO use boost log
-ConsoleWriter out;
 
 /// adjacent edges thresholder based on dot product
 class DotThresholder
@@ -24,7 +20,7 @@ class DotThresholder
 
 public:
 	/// @param	in_poly		input polygon
-	DotThresholder(Pts_t const &in_poly)
+	explicit DotThresholder(Pts_t const &in_poly)
 	{
 		Pts_t unit_directions;
 		// 1. compute normalised directions
@@ -64,60 +60,78 @@ public:
 	}
 };
 
+/// adapted thresholder 
+///	(NOT decorator since interface differs)
+class AdaptedThresholder
+	: public DotThresholder
+{
+	static const auto MAX_ITERATIONS = 10;
+public:
+	explicit AdaptedThresholder(Pts_t const &in_poly)
+		: DotThresholder(in_poly)
+	{
+	}
+
+	/// filter vertices adaptively until num_corners vertices is found
+	/// @param	num_corners		number of vertices to search for
+	/// @return	indices of vertices that are corners (those small enough with the num_corners count)
+	Idxs_t filter(int num_corners)
+	{
+		// DOT product range
+		dvec2 cos_range(-1, 1);
+
+		Idxs_t idxs;
+		int i = 0;
+		// compute thresholds
+		do
+		{
+			auto threshold = (cos_range.x + cos_range.y)*0.5;
+			idxs = DotThresholder::filter(threshold);
+			auto num_idxs = idxs.size();
+
+			// adapt thresholds
+			if (num_idxs > num_corners)
+			{
+				// more vertices classified as corners - 
+				//	reduce threshold, increasing angle between two edges
+				cos_range.y = threshold;
+			}
+			else if (num_idxs < num_corners)
+			{
+				// less vertices classified -
+				//	increase threshold, allowing adjacent edges with greater angle in between
+				cos_range.x = threshold;
+			}
+
+			cerr << "**iteration " << i << endl;
+			++i;
+		} while (idxs.size() != num_corners && i < MAX_ITERATIONS);
+
+		if (i == MAX_ITERATIONS && idxs.size() != num_corners)
+			throw std::runtime_error("failed to detect quad shape!");
+
+		return idxs;
+
+	}
+};
+
 
 void fitPolyAdaptive(Pts_t const &in_poly, Pts_t &out_poly)
 {
 	const auto NUM_CORNERS = 4;
-	const auto MAX_ITERATIONS = 10;
-	
-
 	// preconditions: input has more than 4 corners, otherwise there is no paint
 	assert(in_poly.size() > NUM_CORNERS);
-
-	// DOT product range
-	dvec2 cos_range(-1, 1);
-
 	// first thing - clear out poly
 	out_poly.clear();
-	
 
-	DotThresholder thresholder(in_poly);
-	Idxs_t idxs;
-	int i = 0;
-	// compute thresholds
-	do
-	{
-		auto threshold = (cos_range.x + cos_range.y)*0.5;
-		idxs = thresholder.filter(threshold);
-		auto num_idxs = idxs.size();
+	// find corner indices
+	AdaptedThresholder thresholder(in_poly);
+	auto idxs = thresholder.filter(NUM_CORNERS);
 
-		// adapt thresholds
-		if (num_idxs > NUM_CORNERS)
-		{
-			// more vertices classified as corners - 
-			//	reduce threshold, increasing angle between two edges
-			cos_range.y = threshold;
-		}
-		else if (num_idxs < NUM_CORNERS)
-		{
-			// less vertices classified -
-			//	increase threshold, allowing adjacent edges with greater angle in between
-			cos_range.x = threshold;
-		}
-
-		cerr << "**iteration " << i << endl;
-		++i;
-	} while (idxs.size() != NUM_CORNERS && i < MAX_ITERATIONS);
-
-	if (i == MAX_ITERATIONS && idxs.size() != NUM_CORNERS)
-		throw std::runtime_error("failed to detect quad shape!");
-
-
-	// push points/vertices that passed the test
+	// using indices put found vertices into a polygon
 	for (auto &idx: idxs)
 	{
 		out_poly.push_back(in_poly[idx]);
 	}
-
 }
 
