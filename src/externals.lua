faf13ee0876725219a5ff83b5b65ab3190cbfcb7
Mike Vasiljevs <mike.vasiljevs@ulsee.com>
2019-02-02 15:29:22 +0000

    *port to vs2017 *adjustments made to work with (su) park generator plugin

--- src/externals.lua
+++ src/externals.lua
@@ -1,11 +1,11 @@
 BOOST_INC_DIR	= "../externals/builds-boost/include/boost-1_69"
 BOOST_LIB_DIR	= "../externals/builds-boost/lib"
 
-RUBYSDK_INC_DIR	= "../externals/builds-su-ruby-api/include/ruby"
-RUBYSDK_LIB_DIR	= "../externals/builds-su-ruby-api/lib/win32"
-RUBYUTILS_DIR	=  "../ThirdParty/RubyUtils"
+RUBYSDK_INC_DIR	= path.getabsolute("../externals/builds-su-ruby-api/include/ruby")
+RUBYSDK_LIB_DIR	= path.getabsolute("../externals/builds-su-ruby-api/lib/win32")
+RUBYUTILS_DIR	= path.getabsolute("../ThirdParty/RubyUtils")
 
 GLM_INC_DIR		= "../externals/builds-glm/include"
 
 GTE_INC_DIR		= "../externals/builds-gte/include"
-GTE_LIB_DIR		= "../externals/builds-gte/lib"
+GTE_LIB_DIR		= path.getabsolute("../externals/builds-gte/lib")
--- src/proj_lib_gte_adapter_premake5.lua
+++ src/proj_lib_gte_adapter_premake5.lua
@@ -1,7 +1,10 @@
 ----------------------------------------------------------------
 -- GTE project
 ----------------------------------------------------------------
+-- NOTE externals (framework paths) needed since this one can be included from other projects
+dofile "externals.lua"
 
+print ("gte project")
 	project "GteLib"
 		kind "StaticLib"		
 		flags { "StaticRuntime" }
@@ -12,7 +15,7 @@
 			"gte/*",
 			"shared/shared-geometry.*",
 		}
-						
+			
 		includedirs {	GTE_INC_DIR,
-						GLM_DIR,
+						GLM_INC_DIR,
 		}
